a992ea9a1ffb9d6cce5fb849de306dbac37a421f
Ivan Lapunka <ivan_lapunka@gmail.com>
2023-03-01 18:34:31 +0300

    Changes

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = number / 1000 + (number - (number / 1000) * 1000) / 100 + number % 10 + (number % 100)/10;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+    public static boolean first = true;
     public void revert(int number) {
+        System.out.print(number % 10);
+        number /= 10;
+        System.out.print(number % 10);
+        number /= 10;
+        System.out.println(number % 10);
     }
 }
